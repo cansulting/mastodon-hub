8e3ddbef7352bee6574539246b787f987461e876
Jhoemar <jhoemar_pagao@yahoo.com>
2021-11-10 19:52:48 +0800

    added tests for timeline

--- tests/timeline_test.go
+++ tests/timeline_test.go
@@ -7,9 +7,10 @@ import (
 
 const testHost = "noc.social"
 
+// test loading timeline content
 func TestLoad(t *testing.T) {
 	handler := channel.Load(testHost)
-	json, err := handler.RetrieveLocalTimelines()
+	json, err := handler.RetrieveLocalTimelines(true)
 	if err != nil {
 		t.Error(err)
 		return
